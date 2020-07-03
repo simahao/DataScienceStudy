9fea585053d6968575a71b53959d26b936e88ce5
simahao <simahao@sohu.com>
2020-07-03 21:27:04 +0800

    sync

--- windwaves/bulletscreen.py
+++ windwaves/bulletscreen.py
@@ -3,6 +3,7 @@ import time
 
 import numpy as np
 import pandas as pd
+
 from selenium import webdriver
 from selenium.webdriver.chrome.webdriver import WebDriver
 
@@ -20,7 +21,7 @@ def get_windwaves_bullet_screen(browser: WebDriver, url_month, url_day, url_var1
             data = browser.page_source
             data = data.replace('<html><head></head><body><pre style="word-wrap: break-word; white-space: pre-wrap;">', '')
             data = data.replace('</pre></body></html>', '')
-            json_data = json.loads(data) 
+            json_data = json.loads(data)
             rows = json_data['data']['items']
             contents = [row.get('content') for row in rows]
             thumb_up = [row.get('v2_up_count') for row in rows]
@@ -37,7 +38,7 @@ def get_windwaves_bullet_screen(browser: WebDriver, url_month, url_day, url_var1
         print(e)
         print("getting the {} page bullet screen:FAILED".format(step + 1))
         print("get bullet screen data finished")
-    return one_episode_bs 
+    return one_episode_bs
 
 if __name__ == "__main__":
     url_info = np.array(\
@@ -47,7 +48,7 @@ if __name__ == "__main__":
     browser = webdriver.Chrome()
     for i in range(url_info.shape[0]):
         ds = get_windwaves_bullet_screen(browser=browser, url_month=url_info[i][0], \
-            url_day=url_info[i][1], url_var1=url_info[i][2], url_var2=url_info[i][3]) 
+            url_day=url_info[i][1], url_var1=url_info[i][2], url_var2=url_info[i][3])
         if i == 0:
             # new file at first time
             ds.to_csv('./windwaves/data/bullet.csv', index=False)
